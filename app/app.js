08580135fb4342b86c708bf6eaf7eddb0ca2f2d2
David Llop <llop-no-spam-please@protonmail.com>
2016-02-05 21:16:50 +0100

    Add angular-translate and translate application.

--- app/app.js
+++ app/app.js
@@ -5,8 +5,26 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'pascalprecht.translate'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+.config(function($translateProvider) {
+  $translateProvider
+    .useStaticFilesLoader({
+      prefix: 'l10n/',
+      suffix: '.json'
+    })
+    .useSanitizeValueStrategy('escaped')
+    .registerAvailableLanguageKeys(['en', 'es'], {
+      'en_*': 'en',
+      'es_*': 'es'
+      // When adding more languages, do not forget to add them for "moment"
+      // in the vendor.js section of gulpfile.js
+    })
+    .fallbackLanguage('en')
+    //.determinePreferredLanguage();
+    .preferredLanguage('ca')
+});
